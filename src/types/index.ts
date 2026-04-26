39f0377a05ee2533cb7ce69f662226a9fcbef21e
BemnetKebede <bemnet026@gmail.com>
2026-04-26 08:47:18 +0300

    The Project Architecture & Types

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatarUrl?: string;
+  createdAt: Date | string;
+}
+
+export interface Product {
+  sku: string;
+  name: string;
+  description?: string;
+  price: number;
+  stockQuantity: number;
+  imageUrl?: string;
+}
+
+export type TransactionType = 'added' | 'removed';
+
+export interface Transaction {
+  id: string;
+  productSku: string;
+  type: TransactionType;
+  amount: number;
+  timestamp: Date | string;
+}
